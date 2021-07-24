7140581efece871332b0726fef9c945205abb7d6
M.Ramezani <68474204+MotyRamezani@users.noreply.github.com>
2021-07-25 01:15:27 +0430

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='babelnetpy',
+    packages = ['babelnetpy'],
+    version='0.1.0',
+    description='it extendings BabelNet HTTP API for python 3',
+    long_description="README.md",
+    author='jackee777',
+    install_requires=['urllib'],
+    url='https://github.com/jackee777/pybabelnet'
+)
